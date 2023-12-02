b58d307a6bd2804794c81ea4fd413c32f0338332
roLLlyTHuK <ircvipua@gmail.com>
2023-12-02 15:12:04 +0200

    re-refactoring on useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,125 @@
-import React, { useReducer } from 'react';
+import React, { useState } from 'react';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
-const initialState = { good: 0, neutral: 0, bad: 0 };
-
-const reducer = (state, action) => {
-  switch (action.type)
-  {
-    case 'good':
-      return { ...state, good: state.good + 1 };
-    case 'neutral':
-      return { ...state, neutral: state.neutral + 1 };
-    case 'bad':
-      return { ...state, bad: state.bad + 1 };
-    default:
-      return new Error(`Undefined type: ${action.type}`);
-  }
-};
-
 export const App = () => {
-  const [feedback, dispatch] = useReducer(reducer, initialState);
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
 
   const countTotalFeedback = () => {
     return feedback.good + feedback.neutral + feedback.bad;
   };
 
+  //  const countTotalFeedback = () => good + neutral + bad;
+  
   const countPositiveFeedbackPercentage = () => {
     const totalFeedback = countTotalFeedback();
     return totalFeedback === 0 ? 0 : Math.round((feedback.good / totalFeedback) * 100);
+    //  return totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
   };
 
   const handleFeedback = (type) => {
-    dispatch({ type: type });
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
+    //  switch (type) {
+    //   case 'good':
+    //     setGood((prevGood) => prevGood + 1);
+    //     break;
+    //   case 'neutral':
+    //     setNeutral((prevNeutral) => prevNeutral + 1);
+    //     break;
+    //   case 'bad':
+    //     setBad((prevBad) => prevBad + 1);
+    //     break;
+    //   default:
+    //     break;
+    // }
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <div>
+      <Section title="Please leave your feedback">
+        <FeedbackOptions options={Object.keys(feedback)} onLeaveFeedback={handleFeedback} />
+         {/* <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} /> */}
+      </Section>
+
+      {totalFeedback > 0 ? (
+        <Section title="Statistics">
+          <Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} total={totalFeedback} positivePercentage={positivePercentage} />
+          {/* <Statistics good={good} neutral={neutral} bad={bad} total={totalFeedback} positivePercentage={positivePercentage} /> */}
+        </Section>
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
 };
 
-const totalFeedback = countTotalFeedback();
-const positivePercentage = countPositiveFeedbackPercentage();
+/*============================|useReducer|===========================*/
+// import React, { useReducer } from 'react';
+// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+// import { Notification } from './Notification/Notification';
+// import { Section } from './Section/Section';
+// import { Statistics } from './Statistics/Statistics';
 
-return (
-        <div>
-          <Section title="Please leave your feedback">
-            <FeedbackOptions options={Object.keys(feedback)} onLeaveFeedback={handleFeedback} />
-          </Section>
+// const initialState = { good: 0, neutral: 0, bad: 0 };
 
-        {totalFeedback > 0 ? (
-          <Section title="Statistics">
-            <Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} total={totalFeedback} positivePercentage={positivePercentage} />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-       </div>
-      ); 
-};
+// const reducer = (state, action) => {
+//   switch (action.type)
+//   {
+//     case 'good':
+//       return { ...state, good: state.good + 1 };
+//     case 'neutral':
+//       return { ...state, neutral: state.neutral + 1 };
+//     case 'bad':
+//       return { ...state, bad: state.bad + 1 };
+//     default:
+//       return new Error(`Undefinet type: ${action.type}`);
+//   }
+// };
+
+// export const App = () => {
+//   const [feedback, dispatch] = useReducer(reducer, initialState);
+
+//   const countTotalFeedback = () => {
+//     return feedback.good + feedback.neutral + feedback.bad;
+//   };
+
+//   const countPositiveFeedbackPercentage = () => {
+//     const totalFeedback = countTotalFeedback();
+//     return totalFeedback === 0 ? 0 : Math.round((feedback.good / totalFeedback) * 100);
+//   };
+
+//   const handleFeedback = (type) => {
+//     dispatch({ type: type });
+// };
+
+// const totalFeedback = countTotalFeedback();
+// const positivePercentage = countPositiveFeedbackPercentage();
+
+// return (
+//   <div>
+//     <Section title="Please leave your feedback">
+//       <FeedbackOptions options={Object.keys(feedback)} onLeaveFeedback={handleFeedback} />
+//     </Section>
+
+//   {totalFeedback > 0 ? (
+//     <Section title="Statistics">
+//       <Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} total={totalFeedback} positivePercentage={positivePercentage} />
+//     </Section>
+//   ) : (
+//     <Notification message="There is no feedback" />
+//   )}
+// </div>
+// ); 
+// };
